07e410c49177c16aa8ed345cd86f64fb2f65757b
Carol Casas <124410526+CarolCasas@users.noreply.github.com>
2023-11-07 19:17:26 -0700

    07/11/23

--- Parcial3/registrarMaterias.php
+++ Parcial3/registrarMaterias.php
@@ -15,16 +15,17 @@
     <center><h1>Registrar la materia</h1></center>
         <div class="row">
             <div class="col-12 card p-4">
-                <form action="">
+                <form action="guardarMateria.php" method="POST">
                     <div class="form-group">
-                        <label for="nombre">Nombre: </label>
+                        <i><label for="nombre">Nombre: </label></i>
                         <input type="text" class="form-control" name="nombre" placeholder="Teclea la materia:">
                     </div><br>
                     <div class="form-group">
-                        <label for="semestre">Semestre: </label>
+                        <i><label for="semestre">Semestre: </label></i>
                         <input type="text" class="form-control" name="semestre" placeholder="Teclea el semestre:">
                     </div><br>
                     <div class="form-group">
+                        <i><label for="especialidad">Especialidad: </label></i>
                         <select name="especialidad" class="form-control">
                         <option value="">Seleccione la especialidad: </option>
                         <option value="PROGRAMACION">PROGRAMACION</option>
@@ -32,7 +33,12 @@
                         <option value="OFIMATICA">OFIMATICA</option>
                         <option value="CONSTRUCCION">CONSTRUCCION</option>
                         <option value="ELECTRONICA">ELECTRONICA</option>
+                        <option value="NO_CUENTA_CON_ESPECIALIDAD">NO CUENTA CON ESPECIALIDAD</option>
                     </select>
+                    </div>
+                    <div><br>
+                        <center><input type="submit" class="btn btn-primary" value="Registrar">
+                        <a href="ConsultarMaterias.php" class="btn btn-danger">Cancelar</a></center>
                     </div>
                 </form>
             </div>
